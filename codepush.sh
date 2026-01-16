6b9dde2cbe5beef94aca81b296c96739bc5a155e
udaypoudel <udaypoudel3434@gmail.com>
2026-01-16 09:51:58 +0545

    Add codepush script

---
+++ codepush.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#pushing the linux file to the github account 
+#practicing ......
+# just a random codeeee/texttt
+# that adds two numbers
+
+    echo " Enter a number "
+    read a 
+    echo "enter second number"
+    read b 
+    sum=$(($a + $b))
+    echo " sum is $sum "
+    
